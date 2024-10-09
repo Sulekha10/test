1447d17239c50e333ef469cd10599c1b0d722248
Sulekha10 <sulekhapatelkatni21@gmail.com>
2024-10-09 17:48:39 +0530

    resolving conflict

--- hello.py
+++ hello.py
@@ -1,18 +1,12 @@
 print("Hello , my name is Sulekha Patel")
 greet = input("Enter resion : ")
-<<<<<<< HEAD
+
 if greet == "indian identity":
-=======
-if greet == "indian Person":
->>>>>>> 768782158a9e482ac8cb1a7dd68ebd40e30db9bf
     print("namaste..")
     print("how is going")
     print("which state you are from")
 else:
     print("hello..")
-<<<<<<< HEAD
     print("nice to meet you")
     print("how are you?")
-=======
-    
->>>>>>> 768782158a9e482ac8cb1a7dd68ebd40e30db9bf
+
