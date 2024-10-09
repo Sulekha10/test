71cceaaf092982ab33026b1132edb0d1f26dd24f
Sulekha10 <sulekhapatelkatni21@gmail.com>
2024-10-09 15:44:58 +0530

    asking about

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ print("Hello , my name is Sulekha Patel")
 greet = input("Enter resion : ")
 if greet == "indian":
     print("namaste..")
+    print("how is going")
 else:
     print("hello..")
     
